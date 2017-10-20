af58a22f58a45c3bb49e31997ca3a7c69372c030
yama@muteki <yama@muteki.com>
2017-10-20 15:53:17 +0900

    Hide balloon hint for ruby

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore ~/.gitignore
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
+ln -snfv ~/dotfiles/.vimrc ~/.vimrc
+ln -snfv ~/dotfiles/.gvimrc ~/.gvimrc
+ln -snfv ~/dotfiles/.gitconfig ~/.gitconfig
+ln -snfv ~/dotfiles/.gitignore ~/.gitignore
+ln -snfv ~/dotfiles/.bash_profile ~/.bash_profile
+ln -snfv ~/dotfiles/.bashrc ~/.bashrc
+ln -snfv ~/dotfiles/.tigrc ~/.tigrc
+
+mkdir -p ~/.vim/after/ftplugin
+ln -snfv ~/dotfiles/ruby.vim ~/.vim/after/ftplugin/ruby.vim
 
 ln -snfv ~/.vim ~/.config/nvim/
 ln -snfv ~/.vimrc ~/.config/nvim/init.vim
